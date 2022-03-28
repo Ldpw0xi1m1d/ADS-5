b7c88a55f847bfed5d02f82945674108d7a0b333
Ldpw0xi1m1d <99180755+Ldpw0xi1m1d@users.noreply.github.com>
2022-03-28 21:56:15 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -1,7 +1,7 @@
 #ifndef INCLUDE_TSTACK_H_
 #define INCLUDE_TSTACK_H_
 
-template<typename T, int size>
+template<typename T,int size>
 
 class TStack {
 private:
@@ -12,29 +12,29 @@ T* mas;
 public:
 TStack(int _MaxSize = 15) {
 MaxSize = _MaxSize;
-size = 0;
+size=0;
 mas = new T[MaxSize];
 }
 
 void Push(T a) {
-if (isFull())   throw 1;
+if (isFull()) throw 1;
 mas[size] = a;
 size++;
 }
 
 T Pop() {
-if (isEmpty())  throw  2;
+if (isEmpty()) throw  2;
 size--;
 return mas[size];
 
 }
-T Top() { if (isEmpty()) throw 3; return mas[size - 1]; }
+T Top() {if (isEmpty()) throw 3; return mas[size - 1]; }
 
-~TStack() { delete[] mas; }
+~TStack() {delete[] mas;}
 
-bool isEmpty() { if (size == 0) return true; else return false; }
+bool isEmpty() {if (size == 0) return true; else return false; }
 
-bool isFull() { if (size == MaxSize) return true; else return false; }
+bool isFull() {if (size == MaxSize) return true; else return false; }
 
 void clearTStack() { size = 0; }
 };
