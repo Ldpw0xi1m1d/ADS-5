166f73885994156c0a13942c7862e518209a98c1
Ldpw0xi1m1d <99180755+Ldpw0xi1m1d@users.noreply.github.com>
2022-03-28 21:48:20 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -3,12 +3,88 @@
 #include <map>
 #include "tstack.h"
 
+int priority(char ch) {
+
+switch (ch) {
+case '(': return 0; break;
+case ')': return 1; break;
+case '+': return 2; break;
+case '-': return 2; break;
+case '*': return 3; break;
+case '/': return 3; break;
+}
+return -1;
+}
+
 std::string infx2pstfx(std::string inf) {
-  // добавьте код
-  return std::string("");
+
+string pstfx = "";
+TStack <char,5> stack1;
+
+stack1.clearTStack();
+
+string str = '(' + inf + ')';
+for (int i = 0; i < sizeof(str); i++) {
+if (str[i] >= '0' && str[i] <= '9') { pstfx += str[i]; }
+else if (str[i] == '(') { stack1.Push(str[i]); }
+else if (str[i] == ')') {
+while (stack1.Top() != '(') {
+pstfx += stack1.Pop();
+}
+stack1.Pop();
+}
+else if ((str[i] == '+') || (str[i] == '-') || (str[i] == '*') || (str[i] == '/')) {
+pstfx += ' ';
+
+if (priority(stack1.Top()) < priority(str[i])) {
+stack1.Push(str[i]);
+}
+else {
+while (priority(stack1.Top()) >= priority(str[i]))
+pstfx += stack1.Pop();
+stack1.Push(str[i]);
+}
+
+}
+}
+
+return std::string("");
 }
 
 int eval(std::string pref) {
-  // добавьте код
-  return 0;
+
+TStack<int, 5> stack2;
+int first;
+int second;
+int result;
+
+stack2.clearTStack();
+
+for (size_t i = 0; i < pref.size(); i++) {
+if ((pref[i] == '+') || (pref[i] == '-') || (pref[i] == '/') || (pref[i] == '*')) {
+if (stack2.isEmpty()) throw 4;
+second = stack2.Pop();
+if (stack2.isEmpty()) throw 4;
+first = stack2.Pop();
+switch (pref[i]) {
+case '+': result = first + second; break;
+case '-': result = first - second; break;
+case '*': result = first * second; break;
+case '/': result = first / second; break;
+}
+stack2.Push(result);
+}
+if ((pref[i] >= '0') && (pref[i] <= '9')) {
+char* temp;
+result = strtod(&pref[i], &temp);
+stack2.Push(result);
+int j = temp - &pref[i];
+i += j - 1;
+}
+}
+if (stack2.isEmpty()) { throw 6; }
+else { result = stack2.Pop(); }
+if (!(stack2.isEmpty())) { throw 7; }
+
+return result;
 }
