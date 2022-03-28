8db1713c63624c769fd233c3f0881c208f1c76e4
Ldpw0xi1m1d <99180755+Ldpw0xi1m1d@users.noreply.github.com>
2022-03-28 21:45:24 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -1,9 +1,41 @@
-// Copyright 2021 NNTU-CS
 #ifndef INCLUDE_TSTACK_H_
 #define INCLUDE_TSTACK_H_
 
 template<typename T, int size>
-class TStack {
 
+class TStack {
+private:
+int size;
+int MaxSize;
+T* mas;
+
+public:
+TStack(int _MaxSize = 15) {
+MaxSize = _MaxSize;
+size = 0;
+mas = new T[MaxSize];
+}
+
+void Push(T a) {
+if (isFull())   throw 1;
+mas[size] = a;
+size++;
+}
+
+T Pop() {
+if (isEmpty())  throw  2;
+size--;
+return mas[size];
+
+}
+T Top() { if (isEmpty()) throw 3; return mas[size - 1]; }
+
+~TStack() { delete[] mas; }
+
+bool isEmpty() { if (size == 0) return true; else return false; }
+
+bool isFull() { if (size == MaxSize) return true; else return false; }
+
+void clearTStack() { size = 0; }
 };
-#endif  // INCLUDE_TSTACK_H_
+#endif
